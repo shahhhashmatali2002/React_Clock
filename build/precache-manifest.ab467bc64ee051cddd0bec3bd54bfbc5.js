b24bf6183e94ce4c345fc113a21c8fc093eb7d37
Hashmat Ali Shah <89321559+shahhhashmatali2002@users.noreply.github.com>
2022-03-18 02:06:28 +0500

    Add files via upload

---
+++ build/precache-manifest.ab467bc64ee051cddd0bec3bd54bfbc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8bf23ca9930b439d3e8bd8faaee3ca",
+    "url": "/React_Clock/index.html"
+  },
+  {
+    "revision": "22cc80ec9ac28f243d49",
+    "url": "/React_Clock/static/css/main.447cbb6b.chunk.css"
+  },
+  {
+    "revision": "2e776a451d1a1cb9cf5c",
+    "url": "/React_Clock/static/js/2.bcf67355.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/React_Clock/static/js/2.bcf67355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22cc80ec9ac28f243d49",
+    "url": "/React_Clock/static/js/main.50c375e7.chunk.js"
+  },
+  {
+    "revision": "deac288f857a3e2d654a",
+    "url": "/React_Clock/static/js/runtime-main.e66e8ff9.js"
+  }
+]);
